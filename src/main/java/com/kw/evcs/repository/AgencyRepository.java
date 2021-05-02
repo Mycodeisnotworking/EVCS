0a05bcf8267d9fa1436f4f8f341728f9d9822b59
Korea-subinpark <subinpark.re@gmail.com>
2021-05-02 14:08:01 +0900

    entity, repository 추가

---
+++ src/main/java/com/kw/evcs/repository/AgencyRepository.java
@@ -0,0 +1,7 @@
+package com.kw.evcs.repository;
+
+import com.kw.evcs.entity.Agency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgencyRepository extends JpaRepository<Agency, Long> {
+}
